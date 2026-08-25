898cb4357f07e1821313f5314e8959f02cbe1c9e
Clint <136924523+cgado2004@users.noreply.github.com>
2026-08-25 16:28:52 +0800

    Update Program.cs

--- Gado_CQMS/Program.cs
+++ Gado_CQMS/Program.cs
@@ -8,9 +8,7 @@ namespace Gado_CQMS
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
